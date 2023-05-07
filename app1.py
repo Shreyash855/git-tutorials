073f439ef02fa021649dbf438f7e39f79b9064ed
Shreyash855 <63160674+Shreyash855@users.noreply.github.com>
2023-05-07 13:02:53 +0530

    Version-2

--- app1.py
+++ app1.py
@@ -7,3 +7,6 @@ print("Addition = ",num1+num2)
 print("Subtraction = ",num1-num2)
 print("Multiplication = ",num1*num2)
 print("Division = ",num1/num2)
+print("Modulus = ",num1%num2)
+
+print("Thank you!!!")
